b9a1d22eddbca682ae9de351cc23457a8201947c
Nelson Zamith <nzamith@teclib.com>
2015-07-22 17:38:54 +0200

    adding angular-material as bower dependency

--- frontend/test/karma.conf.js
+++ frontend/test/karma.conf.js
@@ -24,6 +24,8 @@ module.exports = function(config) {
       // bower:js
       'bower_components/angular/angular.js',
       'bower_components/angular-animate/angular-animate.js',
+      'bower_components/angular-aria/angular-aria.js',
+      'bower_components/angular-material/angular-material.js',
       'bower_components/angular-mocks/angular-mocks.js',
       // endbower
       "app/scripts/**/*.js",
