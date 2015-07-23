ad7f701b570e613c3523dd9c6c5ad2793e45f25e
Nelson Zamith <nzamith@teclib.com>
2015-07-23 14:37:16 +0200

    using the new namespaced classes in index.php

--- api/index.php
+++ api/index.php
@@ -2,12 +2,32 @@
 
 require 'vendor/autoload.php';
 
-\API\DB::initCapsule();
+\API\Core\DB::initCapsule();
 $app = new \Slim\Slim();
 
+// List of all plugins
 $app->get('/plugin', function() use ($app) {
 	$app->response->headers->set('Content-Type', 'application/json');
-	echo json_encode(\API\Plugin::with('descriptions', 'authors')->get()->toArray());
+	echo json_encode(\API\Model\Plugin::with('descriptions', 'authors')->get()->toArray());
+});
+
+// 
+$app->get('/plugin/trending', function() use($app) {
+	$app->response->headers->set('Content-Type', 'application/json');
+	// This is the typical mysql query:
+	//    SELECT name, count(name) FROM plugin
+	//    INNER JOIN plugin_download
+	//    ON plugin.id = plugin_download.plugin_id
+	//    GROUP BY name;
+	echo json_encode(new stdClass);
+//	echo json_encode(\API\Model\Plugin::with('description', 'authors'));
+});
+
+
+
+// Star a plugin
+$app->get('/plugin/star', function() use($app) {
+
 });
 
 $app->run();
