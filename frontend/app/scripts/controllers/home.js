8ddba60e12e9a0e90d72a8370bf1669764173b79
Nelson <zamith.nelson@gmail.com>
2015-07-26 20:51:02 +0200

    primary approach to fetching the home resources

--- frontend/app/scripts/controllers/home.js
+++ frontend/app/scripts/controllers/home.js
@@ -8,7 +8,7 @@
  * Controller of the frontendApp
  */
 angular.module('frontendApp')
-  .controller('HomeCtrl', ['API_URL', '$http', '$scope', function (apiUrl, http) {
+  .controller('HomeCtrl', ['API_URL', '$http', '$scope', function (apiUrl, $http, $scope) {
   		$scope.trending = [];
   		$scope.new = [];
   		$scope.popular = [];
@@ -16,13 +16,36 @@ angular.module('frontendApp')
   		$scope.tags = [];
   		$scope.authors = [];
 
-      // $http.get(apiUrl)
-      //   .success(function(data, status, headers, config) {
-  
-      //   });
-  		// $http({
-  		// 	method: 'GET',
-  		// 	url: apiUrl + '/plugin/last';
-  		// })
+  		$http({
+  			method: 'GET',
+  			url: apiUrl + '/plugin/trending'
+  		})
+      .success(function(data, status, headers, config) {
+        $scope.trending = data;
+      });
+
+      $http({
+        method: 'GET',
+        url: apiUrl + '/plugin/popular'
+      })
+      .success(function(data, status, headers, config) {
+        $scope.popular = data;
+      });
+
+      $http({
+        method: 'GET',
+        url: apiUrl + '/plugin/updated'
+      })
+      .success(function(data, status, headers, config) {
+        $scope.updated = data;
+      });
+
+      $http({
+        method: 'GET',
+        url: apiUrl + '/author'
+      })
+      .success(function(data, status, headers, config) {
+        $scope.authors = data;
+      });
 
     }]);
