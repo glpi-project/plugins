de176c9f8ebff955977534aac3f6a8db37d0db86
Nelson Zamith <nzamith@teclib.com>
2015-07-27 17:52:19 +0200

    added single-plugin view/controller, begun working

--- frontend/app/scripts/app.js
+++ frontend/app/scripts/app.js
@@ -30,17 +30,25 @@ angular
          templateUrl: "views/home.html",
          controller: 'HomeCtrl'
        })
+       // plugin State
+       .state('plugin', {
+         url: '/plugin/:id',
+         templateUrl: 'views/plugin.html',
+         controller: 'PluginCtrl'
+       })
        // search State
        .state('search', {
          url: '/search/:val',
          templateUrl: "views/search.html",
          controller: "SearchCtrl"
        })
+       // submit State
        .state('submit', {
        	 url: '/submit',
        	 templateUrl: "views/submit.html",
        	 controller: "SubmitCtrl"
        })
+       // contact State
        .state('contact', {
        	 url: '/contact',
        	 templateUrl: 'views/contact.html',
---
+++ frontend/app/scripts/controllers/plugin.js
@@ -0,0 +1,41 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name frontendApp.controller:PluginCtrl
+ * @description
+ * # PluginCtrl
+ * Controller of the frontendApp
+ */
+angular.module('frontendApp')
+
+  .directive('voteBox', function() {
+  	return {
+  		restrict: 'E'
+  	};
+  })
+
+  .controller('PluginCtrl', function (API_URL, $scope, $http, $stateParams) {
+  	$scope.plugin = {
+  		authors: {}
+  	};
+
+  	$scope.inlineAuthors = function(authors) {
+  		var _authors = '';
+  		for (var i = 0 ; i < authors.length; i++) {
+  			if (i > 0) {
+  				_authors += ', ';
+  			}
+  			_authors += authors[i].author;
+  		}
+  		return _authors;
+  	};
+
+    $http({
+    	method: 'GET',
+    	url: API_URL + '/plugin/' + $stateParams.id
+    })
+    .success(function(data, status, headers, config) {
+    	$scope.plugin = data;
+    });
+  });
