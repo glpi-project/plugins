06353f79035cb978d6ded8720c6f06477ac08ff3
Nelson Zamith <nzamith@teclib.com>
2015-07-29 09:44:27 +0200

    core code to 3space-indentation

--- api/src/core/DB.php
+++ api/src/core/DB.php
@@ -5,13 +5,13 @@ namespace API\Core;
 use Illuminate\Database\Capsule\Manager as Capsule;
 
 class DB {
-	private static $capsule;
+   private static $capsule;
 
-	public static function initCapsule() {
-		require 'config.php'; // need database credentials
-		self::$capsule = new Capsule;
-		self::$capsule->addConnection($db_settings);
-		self::$capsule->bootEloquent();
-		self::$capsule->setAsGlobal();
-	}
+   public static function initCapsule() {
+      require 'config.php'; // need database credentials
+      self::$capsule = new Capsule;
+      self::$capsule->addConnection($db_settings);
+      self::$capsule->bootEloquent();
+      self::$capsule->setAsGlobal();
+   }
 }
--- api/src/core/Tool.php
+++ api/src/core/Tool.php
@@ -3,21 +3,21 @@
 namespace API\Core;
 
 class Tool {
-	/**
-	 * You can use this method to
-	 * end with a JSON value from
-	 * an endpoint.
-	 */
-	public static function endWithJson($json_value) {
-		global $app;
-		$app->response->headers->set('Content-Type', 'application/json');
-		echo json_encode($json_value);
-	}
+   /**
+    * You can use this method to
+    * end with a JSON value from
+    * an endpoint.
+    */
+   public static function endWithJson($json_value) {
+      global $app;
+      $app->response->headers->set('Content-Type', 'application/json');
+      echo json_encode($json_value);
+   }
 
-	public static function getBody() {
-		global $app;
-		$json = $app->request->getBody();
-		$json = json_decode($json);
-		return $json;
-	}
+   public static function getBody() {
+      global $app;
+      $json = $app->request->getBody();
+      $json = json_decode($json);
+      return $json;
+   }
 }
