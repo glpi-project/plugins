8b8b6b19718758411124941e2e870271e257d9c6
Nelson Zamith <nzamith@teclib.com>
2015-07-29 12:09:43 +0200

    able to display a note

--- frontend/app/scripts/controllers/plugin.js
+++ frontend/app/scripts/controllers/plugin.js
@@ -10,26 +10,27 @@
 angular.module('frontendApp')
 
   .controller('PluginCtrl', function (API_URL, $scope, $http, $stateParams, $sce) {
-  	$scope.plugin = {
-  		authors: {}
-  	};
+    $scope.plugin = {
+      authors: {}
+    };
+    $scope.note = 4.5;
 
-  	$scope.inlineAuthors = function(authors) {
-  		var _authors = '';
-  		for (var i = 0 ; i < authors.length; i++) {
-  			if (i > 0) {
-  				_authors += ', ';
-  			}
-  			_authors += authors[i].author;
-  		}
-  		return _authors;
-  	};
+    $scope.inlineAuthors = function(authors) {
+      var _authors = '';
+      for (var i = 0 ; i < authors.length; i++) {
+        if (i > 0) {
+          _authors += ', ';
+        }
+        _authors += authors[i].author;
+      }
+      return _authors;
+    };
 
     $http({
-    	method: 'GET',
-    	url: API_URL + '/plugin/' + $stateParams.id
+      method: 'GET',
+      url: API_URL + '/plugin/' + $stateParams.id
     })
     .success(function(data, status, headers, config) {
-    	$scope.plugin = data;
+      $scope.plugin = data;
     });
   });
--- frontend/app/scripts/directives/ratebox.js
+++ frontend/app/scripts/directives/ratebox.js
@@ -9,10 +9,60 @@
 angular.module('frontendApp')
   .directive('rateBox', function () {
     return {
-      template: '<div></div>',
       restrict: 'E',
-      link: function postLink(scope, element, attrs) {
-        //element.text('this is the rateBox directive');
+      link: function postLink(scope, element, attrs, controller) {
+        if (typeof(scope.currentNote) === 'undefined')
+          scope.currentNote = 0;
+
+        var stars = controller.getStarsFromNote(scope.currentNote);
+
+        for (var i = 0 ; i < stars.length ; i++) {
+          element.append(stars[i]);
+        }
+      },
+      scope: {
+        currentNote: "=currentNote"
+      },
+      controller: function($scope) {
+        this.getFullStar = function() {
+          return angular.element('<i class="fa fa-star">');
+        };
+
+        this.getHalfStar = function() {
+          return angular.element('<i class="fa fa-star-half-o">');
+        };
+
+        this.getEmptyStar = function() {
+          return angular.element('<i class="fa fa-star-o">');
+        };
+
+        this.getStarsFromNote = function(note) {
+          if (note > 5 || note < 0)
+            note = 0;
+          var stars = [];
+          var lastIsHalf = note % 1 > 0;
+
+          console.log(note, lastIsHalf);
+          note = Math.floor(note);
+
+          for (var i = 0 ; i < note ; i++) {
+            stars.push(this.getFullStar());
+          }
+
+          if (lastIsHalf) {
+            stars.push(this.getHalfStar());
+            i = i + 0.5;
+          }
+
+          if (5 - i >= 1) {
+            var missingStars = Math.floor(5 - i);
+            for (var i = 0 ; i < missingStars ; i++) {
+              stars.push(this.getEmptyStar());
+            }
+          }
+
+          return stars;
+        };
       }
     };
   });
