e31be44bfd628b01fe3e7065b587f1e362f3ba38
Nelson Zamith <nzamith@teclib.com>
2015-07-29 16:36:41 +0200

    reverting  setting in PluginAuthor.php

--- api/src/models/PluginAuthor.php
+++ api/src/models/PluginAuthor.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class PluginAuthor extends Model {
    protected $table = 'plugin_author';
-   //protected $visible = ['author'];
+   protected $visible = ['author'];
 
    public function plugin() {
       return $this->belongsTo('\API\Model\Plugin');
