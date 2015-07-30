7f9a9e8e5f039aacd586ceeace414f07feb19ccb
Nelson Zamith <nzamith@teclib.com>
2015-07-30 14:18:35 +0200

    notebox: you can drag the cursor, and make that move

--- frontend/app/scripts/directives/ratebox.js
+++ frontend/app/scripts/directives/ratebox.js
@@ -28,19 +28,47 @@ angular.module('frontendApp')
         }
 
         // Modify the DOM with the current stars
-        var displayStars = function() {
-          var classes = controller.getClassesFromNote(scope.currentNote);
+        var displayStars = function(note) {
+          var classes = controller.getClassesFromNote(note);
           for (var i = 0 ; i < stars.length ; i++) {
            stars[i].attr('class', classes[i]);
           }
         };
 
+        // Modify
+        var displayStarsHover = function() {
+
+        };
+
+        // Compute the stars height/width
+        controller.starWidth = 25;
+        controller.starHeight = 25;
+ 
+        var halfOrPlain = function(offsetX) {
+          return (controller.starWidth -
+                  (controller.starWidth - offsetX)
+                  <= (controller.starWidth / 2)
+                 ) ? 0.5 : 1;
+        };
+
+        stars.forEach(function(el, i) {
+          el.on('mouseenter', function(ev) {
+            displayStars(i + halfOrPlain(ev.offsetX));
+          });
+          el.on('mousemove', function(ev) {
+            displayStars(i + halfOrPlain(ev.offsetX));
+          });
+          el.on('mouseleave', function(ev) {
+            displayStars(scope.currentNote);
+          });
+        });
+
         // Watch for future modifications of the note
         scope.$watch('currentNote', function() {
-          displayStars();
+          displayStars(scope.currentNote);
         });
         // Create stars for current note
-        displayStars();
+        displayStars(scope.currentNote);
       },
 
       scope: {
@@ -48,45 +76,51 @@ angular.module('frontendApp')
       },
       controller: function($scope) {
         // returns fontawesome class for a full star
-        this.getFullStar = function() {
-          return 'fa fa-star';
+        this.getFullStar = function(hover) {
+          return 'fa fa-star' +(hover?' hover':'');
         };
         // returns fontawesome class for half a star
-        this.getHalfStar = function() {
-          return 'fa fa-star-half-o';
+        this.getHalfStar = function(hover) {
+          return 'fa fa-star-half-o' +(hover?' hover':'');
         };
         // returns fontawesome class for an empty star
-        this.getEmptyStar = function() {
-          return 'fa fa-star-o';
+        this.getEmptyStar = function(hover) {
+          return 'fa fa-star-o' +(hover?' hover':'');
         };
         // returns an array of dom elements
         // which are the stars of the current
         // note
-        this.getClassesFromNote = function(note) {
+        this.getClassesFromNote = function(note, hover) {
           if (note > 5 || note < 0)
             note = 0;
           var stars = [];
           var lastIsHalf = note % 1 > 0;
 
+          var hover = hover ? true : false;
+
           note = Math.floor(note);
 
           for (var i = 0 ; i < note ; i++) {
-            stars.push(this.getFullStar());
+            stars.push(this.getFullStar(hover));
           }
 
           if (lastIsHalf) {
-            stars.push(this.getHalfStar());
+            stars.push(this.getHalfStar(hover));
             i = i + 0.5;
           }
 
           if (5 - i >= 1) {
             var missingStars = Math.floor(5 - i);
             for (var i = 0 ; i < missingStars ; i++) {
-              stars.push(this.getEmptyStar());
+              stars.push(this.getEmptyStar(hover));
             }
           }
 
           return stars;
+        };
+
+        this.getHoverClassesFromNote = function(note) {
+
         };
       }
     };
