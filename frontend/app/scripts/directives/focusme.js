150f215a6d6aa80bd46366791902160576613bb2
Alexandre Delaunay <adelaunay@teclib.com>
2015-07-30 16:39:11 +0200

    add search link and focus directive

---
+++ frontend/app/scripts/directives/focusme.js
@@ -0,0 +1,24 @@
+'use strict';
+
+/**
+ * @ngdoc directive
+ * @name frontendApp.directive:rateBox
+ * @description
+ * # rateBox
+ */
+angular.module('frontendApp')
+
+   // From http://stackoverflow.com/questions/14833326/how-to-set-focus-on-input-field
+   .directive('focusMe', function() {
+    return {
+      scope: { trigger: '=focusMe' },
+      link: function(scope, element) {
+        scope.$watch('trigger', function(value) {
+          if(value === true) { 
+              element[0].focus();
+              scope.trigger = false;
+          }
+        });
+      }
+    };
+  });
