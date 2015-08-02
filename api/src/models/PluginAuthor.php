eba49da407cf6d8f48c339ee250a8309c15d5448
Nelson Zamith <nzamith@teclib.com>
2015-08-02 13:46:22 +0200

    fixed a bug with author list

--- api/src/models/PluginAuthor.php
+++ api/src/models/PluginAuthor.php
@@ -7,15 +7,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class PluginAuthor extends Model {
    protected $table = 'plugin_author';
-   protected $visible = ['author'];
+   protected $visible = ['author', 'plugin_count'];
 
    public function plugin() {
       return $this->belongsTo('\API\Model\Plugin');
    }
 
    public function scopeMostActive($query, $limit=10) {
-      $query->select(['plugin_author.author', DB::raw('COUNT(author) as plugin_count')])
-            ->groupBy('author')
+      $query->select(['*', 'plugin_author.author', DB::raw('COUNT(plugin_author.plugin_id) as plugin_count')])
+            ->groupBy('plugin_author.author')
             ->orderBy('plugin_count', 'DESC')
             ->take($limit);
       return $query;
