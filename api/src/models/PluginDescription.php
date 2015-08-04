ffc0b84881b0dea770959d3056fc487fcf908103
Nelson Zamith <nzamith@teclib.com>
2015-08-04 23:07:55 +0200

    disabling timestamps in PluginDescription

--- api/src/models/PluginDescription.php
+++ api/src/models/PluginDescription.php
@@ -7,6 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class PluginDescription extends Model {
    protected $table = 'plugin_description';
    protected $visible = ['short_description', 'long_description', 'lang'];
+   public $timestamps = false;
  
    public function plugin() {
       return $this->belongsTo('\API\Model\Plugin');
