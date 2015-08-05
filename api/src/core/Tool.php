32cd3ff826c94d16895afd017076523f80d5d072
Nelson Zamith <nzamith@teclib.com>
2015-08-05 18:11:16 +0200

    creating a method in tool to make url slugs

--- api/src/core/Tool.php
+++ api/src/core/Tool.php
@@ -15,10 +15,34 @@ class Tool {
       echo json_encode($json_value);
    }
 
+   /**
+    * You can use this method in
+    * an endpoint to get the value
+    * of the JSON body being given
+    * with a request
+    */
    public static function getBody() {
       global $app;
       $json = $app->request->getBody();
       $json = json_decode($json);
       return $json;
+   }
+
+   public static $url_slug_authorized = [
+      'a','b','c','d','e','f','g','h','i',
+      'j','k','l','m','n','o','p','q','r',
+      's','t','u','v','w','x','y','z'
+   ];
+
+   public static function getUrlSlug($free_string) {
+      $free_string = strtolower($free_string);
+      $out = '';
+      for ($i = 0 ; $i < strlen($free_string) ; $i++) {
+        if (!in_array($free_string[$i], self::$url_slug_authorized))
+          $out .= '-';
+        else
+          $out .= $free_string[$i];
+      }
+      return $out;
    }
 }
