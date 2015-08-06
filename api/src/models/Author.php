9a2908785dfbb805245ffb580fb5c0b0fe8dbd43
Nelson Zamith <nzamith@teclib.com>
2015-08-06 14:13:22 +0200

    removed a commented method which has been overriden

--- api/src/models/Author.php
+++ api/src/models/Author.php
@@ -14,14 +14,6 @@ class Author extends Model {
       return $this->belongsToMany('\API\Model\Plugin', 'plugin_author');;
    }
 
-   // public function scopeMostActive($query, $limit=10) {
-   //    $query->select(['*', 'plugin_author.author', DB::raw('COUNT(plugin_author.plugin_id) as plugin_count')])
-   //          ->groupBy('plugin_author.author')
-   //          ->orderBy('plugin_count', 'DESC')
-   //          ->take($limit);
-   //    return $query;
-   // }
-
    public function scopeMostActive($query, $limit = 10) {
       $query->select(['author.id', 'author.name', DB::raw('COUNT(plugin_author.plugin_id) as plugin_count')])
             ->leftJoin('plugin_author', 'author.id', '=', 'plugin_author.author_id')
