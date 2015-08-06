311051dcfa0ae0f1083298f4d68b67252cd58cb7
Nelson Zamith <nzamith@teclib.com>
2015-08-06 09:43:13 +0200

    removed previous property of class now unused

--- api/src/core/Tool.php
+++ api/src/core/Tool.php
@@ -28,25 +28,6 @@ class Tool {
       return $json;
    }
 
-   public static $url_slug_authorized = [
-      'a','b','c','d','e','f','g','h','i',
-      'j','k','l','m','n','o','p','q','r',
-      's','t','u','v','w','x','y','z'
-   ];
-
-   // public static function getUrlSlug($free_string) {
-   //    $free_string = strtolower($free_string);
-   //    $out = '';
-   //    for ($i = 0 ; $i < strlen($free_string) ; $i++) {
-   //      var_dump(substr($free_string, $i));
-   //      if (!in_array($free_string[$i], self::$url_slug_authorized))
-   //        $out .= '-';
-   //      else
-   //        $out .= $free_string[$i];
-   //    }
-   //    return $out;
-   // }
-
    public static function getUrlSlug($free_string) {
     $free_string = strtolower($free_string);
     $free_string = mb_ereg_replace ( '[^a-z]' , '-' , $free_string);
