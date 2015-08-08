ba8436564a03c065fc52fa8bf62b1d31f849fbbe
Nelson Zamith <nzamith@teclib.com>
2015-08-08 14:07:52 +0200

    added a lang picker controller + view + sass

---
+++ frontend/app/scripts/controllers/langswitcher.js
@@ -0,0 +1,13 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name frontendApp.controller:LangswitcherCtrl
+ * @description
+ * # LangswitcherCtrl
+ * Controller of the frontendApp
+ */
+angular.module('frontendApp')
+  .controller('LangSwitcherCtrl', function () {
+    console.log('this is the lang switcher controller');
+  });
