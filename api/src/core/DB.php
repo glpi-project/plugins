d21f657ac8e0fab25c8a3753a4a4d95e9a8187fd
Nelson Zamith <nzamith@teclib.com>
2015-08-10 14:23:37 +0200

    added query parameters in the query log

--- api/src/core/DB.php
+++ api/src/core/DB.php
@@ -17,9 +17,21 @@ class DB {
       self::$capsule->setAsGlobal();
       self::$capsule->bootEloquent();
       if ($log_queries) {         
-         self::$capsule->getEventDispatcher()->listen('illuminate.query', function($query) {
+         self::$capsule->getEventDispatcher()->listen('illuminate.query', function($query, $params) {
             $log = fopen('../misc/illuminate_queries.log', 'a+');
-            fwrite($log, date('Y/m/d H:i:s') . ' [QUERY] : ' . $query . "\n");
+            fwrite($log, date('Y/m/d H:i:s') . ' [QUERY] : ' . $query);
+            fwrite($log, ' [with params ');
+            $afterFirst = false;;
+            foreach ($params as $param) {
+               if ($afterFirst) {
+                  fwrite($log, ', ');
+               }
+               fwrite($log, $param);
+               if (!$afterFirst) {
+                  $afterFirst = true;
+               }
+            }
+            fwrite($log, "]\n");
             fclose($log);
          });
       }
