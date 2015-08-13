b41ee5d6a53994c6707e5387015699df43da8d4c
Alexandre Delaunay <adelaunay@teclib.com>
2015-08-13 09:48:44 +0200

    coding standards for locales (3spaces)

--- frontend/app/scripts/translations/en.js
+++ frontend/app/scripts/translations/en.js
@@ -1,46 +1,46 @@
 'use strict';
 
 angular
-  .module('frontendApp')
-  .config(function($translateProvider) {
-    $translateProvider
-      .translations('en', {
-        ON: "on",
-        SLOGAN: "Extend your GLPI with plugins",
-        TRENDING: "Trending",
-        TRENDING_SUB: "Often downloaded this month",
-        NEW: "New",
-        NEW_SUB: "Most recent in the catalog",
-        POPULAR: "Popular",
-        POPULAR_SUB: "With the most unique installs",
-        UPDATED: "Updated",
-        UPDATED_SUB: "Recently updated plugins",
-        TAGS: "Tags",
-        TAGS_SUB: "With the highest number of plugin",
-        AUTHORS: "Authors",
-        AUTHORS_SUB: "With the highest number of contributions",
-        NAV_FEATURED: "Featured",
-        NAV_ALLPLUGINS: "All plugins",
-        NAV_SEARCH: "Search",
-        NAV_SUBMIT_A_PLUGIN: "Submit a plugin",
-        NAV_CONTACT: "Contact",
-        SEARCHBAR_PLACEHOLDER: "Search",
-        VERSION: "Version",
-        COMPATIBLE_WITH: "Compatible with",
-        HOMEPAGE: "Homepage",
-        ADDED: "Added",
-        BY: "By",
-        PLUGIN_CONTRIBUTED_TO: "contributed to/worked on those plugins",
-        PLUGIN_TAGGED_WITH: "Plugins tagged with",
-        QUESTION_SUGGESTION_PROBLEM: "Any question, suggestion, problem ?",
-        FIRSTNAME: "First name",
-        LASTNAME: "Last name",
-        EMAIL: "Email",
-        SUBJECT: "Subject",
-        YOUR_MESSAGE: "Your message",
-        SUBMIT: "Submit",
-        SUBMIT_YOUR_PLUGIN: "Submit your plugin!",
-        SUB_SUBMIT_YOUR_PLUGIN: "Please, provide the url describing your plugin.",
-        GLPI_PLUGIN_CREATORS: "GLPI Plugin Creators"
-      });
-  });
+   .module('frontendApp')
+   .config(function($translateProvider) {
+      $translateProvider
+         .translations('en', {
+            ON: "on",
+            SLOGAN: "Extend your GLPI with plugins",
+            TRENDING: "Trending",
+            TRENDING_SUB: "Often downloaded this month",
+            NEW: "New",
+            NEW_SUB: "Most recent in the catalog",
+            POPULAR: "Popular",
+            POPULAR_SUB: "With the most unique installs",
+            UPDATED: "Updated",
+            UPDATED_SUB: "Recently updated plugins",
+            TAGS: "Tags",
+            TAGS_SUB: "With the highest number of plugin",
+            AUTHORS: "Authors",
+            AUTHORS_SUB: "With the highest number of contributions",
+            NAV_FEATURED: "Featured",
+            NAV_ALLPLUGINS: "All plugins",
+            NAV_SEARCH: "Search",
+            NAV_SUBMIT_A_PLUGIN: "Submit a plugin",
+            NAV_CONTACT: "Contact",
+            SEARCHBAR_PLACEHOLDER: "Search",
+            VERSION: "Version",
+            COMPATIBLE_WITH: "Compatible with",
+            HOMEPAGE: "Homepage",
+            ADDED: "Added",
+            BY: "By",
+            PLUGIN_CONTRIBUTED_TO: "contributed to/worked on those plugins",
+            PLUGIN_TAGGED_WITH: "Plugins tagged with",
+            QUESTION_SUGGESTION_PROBLEM: "Any question, suggestion, problem ?",
+            FIRSTNAME: "First name",
+            LASTNAME: "Last name",
+            EMAIL: "Email",
+            SUBJECT: "Subject",
+            YOUR_MESSAGE: "Your message",
+            SUBMIT: "Submit",
+            SUBMIT_YOUR_PLUGIN: "Submit your plugin!",
+            SUB_SUBMIT_YOUR_PLUGIN: "Please, provide the url describing your plugin.",
+            GLPI_PLUGIN_CREATORS: "GLPI Plugin Creators"
+         });
+   });
--- frontend/app/scripts/translations/es.js
+++ frontend/app/scripts/translations/es.js
@@ -1,46 +1,46 @@
 'use strict';
 
 angular
-  .module('frontendApp')
-  .config(function($translateProvider) {
-    $translateProvider
-      .translations('es', {
-        ON: "sur",
-        SLOGAN: "Extender GLPI con plugins",
-        TRENDING: "Tendencias",
-        TRENDING_SUB: "Beaucoup téléchargés ce mois-ci",
-        NEW: "Nuevos",
-        NEW_SUB: "Les plus récents",
-        POPULAR: "Superior",
-        POPULAR_SUB: "Les mieux notés",
-        UPDATED: "Actualizado",
-        UPDATED_SUB: "Derniers mis à jours",
-        TAGS: "Tags",
-        TAGS_SUB: "Aux plugins les plus nombreux",
-        AUTHORS: "Autores",
-        AUTHORS_SUB: "Plus gros contributeurs",
-        NAV_FEATURED: "Featured",
-        NAV_ALLPLUGINS: "All plugins",
-        NAV_SEARCH: "Buscar",
-        NAV_SUBMIT_A_PLUGIN: "Añadir su plugin",
-        NAV_CONTACT: "Contáctenos",
-        SEARCHBAR_PLACEHOLDER: "Buscar",
-        VERSION: "Version",
-        COMPATIBLE_WITH: "Compatible con",
-        HOMEPAGE: "Sitio web",
-        ADDED: "Añadido",
-        BY: "Par",
-        PLUGIN_CONTRIBUTED_TO: "à contribué/travaillé sur ces plugins",
-        PLUGIN_TAGGED_WITH: "Plugins taggés avec",
-        QUESTION_SUGGESTION_PROBLEM: "Una pregunta, una sugerencia, un problema?",
-        FIRSTNAME: "Prénom",
-        LASTNAME: "Nom de famille",
-        EMAIL: "Email",
-        SUBJECT: "Sujet",
-        YOUR_MESSAGE: "Votre message",
-        SUBMIT: "Envoyer",
-        SUBMIT_YOUR_PLUGIN: "Ajouter votre plugin!",
-        SUB_SUBMIT_YOUR_PLUGIN: "Veuillez fournir une URL vers le fichier xml décrivant votre plugin",
-        GLPI_PLUGIN_CREATORS: "GLPI Plugin Creators"
-      });
-  });
+   .module('frontendApp')
+   .config(function($translateProvider) {
+      $translateProvider
+         .translations('es', {
+            ON: "sur",
+            SLOGAN: "Extender GLPI con plugins",
+            TRENDING: "Tendencias",
+            TRENDING_SUB: "Beaucoup téléchargés ce mois-ci",
+            NEW: "Nuevos",
+            NEW_SUB: "Les plus récents",
+            POPULAR: "Superior",
+            POPULAR_SUB: "Les mieux notés",
+            UPDATED: "Actualizado",
+            UPDATED_SUB: "Derniers mis à jours",
+            TAGS: "Tags",
+            TAGS_SUB: "Aux plugins les plus nombreux",
+            AUTHORS: "Autores",
+            AUTHORS_SUB: "Plus gros contributeurs",
+            NAV_FEATURED: "Featured",
+            NAV_ALLPLUGINS: "All plugins",
+            NAV_SEARCH: "Buscar",
+            NAV_SUBMIT_A_PLUGIN: "Añadir su plugin",
+            NAV_CONTACT: "Contáctenos",
+            SEARCHBAR_PLACEHOLDER: "Buscar",
+            VERSION: "Version",
+            COMPATIBLE_WITH: "Compatible con",
+            HOMEPAGE: "Sitio web",
+            ADDED: "Añadido",
+            BY: "Par",
+            PLUGIN_CONTRIBUTED_TO: "à contribué/travaillé sur ces plugins",
+            PLUGIN_TAGGED_WITH: "Plugins taggés avec",
+            QUESTION_SUGGESTION_PROBLEM: "Una pregunta, una sugerencia, un problema?",
+            FIRSTNAME: "Prénom",
+            LASTNAME: "Nom de famille",
+            EMAIL: "Email",
+            SUBJECT: "Sujet",
+            YOUR_MESSAGE: "Votre message",
+            SUBMIT: "Envoyer",
+            SUBMIT_YOUR_PLUGIN: "Ajouter votre plugin!",
+            SUB_SUBMIT_YOUR_PLUGIN: "Veuillez fournir une URL vers le fichier xml décrivant votre plugin",
+            GLPI_PLUGIN_CREATORS: "GLPI Plugin Creators"
+         });
+   });
