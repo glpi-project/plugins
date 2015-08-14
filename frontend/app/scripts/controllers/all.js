0d7ff6052282c6e5804ac0d45abd318e7ddada0a
Nelson Zamith <nzamith@teclib.com>
2015-08-14 15:58:52 +0200

    begun work on that wip

--- frontend/app/scripts/controllers/all.js
+++ frontend/app/scripts/controllers/all.js
@@ -12,14 +12,38 @@ angular.module('frontendApp')
       var grabAllPlugins = function() {
          $http({
             method: "GET",
-            url: API_URL + '/plugin'
+            url: API_URL + '/plugin',
+            headers: {
+               'X-Range': $scope.from+'-'+$scope.to
+            }
          })
-         .success(function(data) {
+         .success(function(data, status, headers) {
             $scope.results = data;
+            var contentRange = /([0-9]+)-([0-9]+)\/([0-9]+)/.exec(headers()['content-range']);
+            var startIndex = parseInt(contentRange[1]);
+            var endIndex = parseInt(contentRange[2]);
+            var maxIndex = parseInt(contentRange[3]);
+            
+            var pageCount = Math.ceil(maxIndex / (endIndex - startIndex));
+            $scope.pages = [];
+            for (var n = 0, i = 0 ; n < pageCount ; n++) {
+               $scope.pages.push({
+                  startIndex: i,
+                  n: n
+               });
+               i += (endIndex - startIndex + 1);
+            }
          });
       };
 
+      $scope.openPage = function(i) {
+         console.log(i);
+      };
+
+      $scope.page = 0;
       $scope.results = [];
+      $scope.from = 0;
+      $scope.to = 14;
 
       grabAllPlugins();
       $scope.$on('languageChange', grabAllPlugins);
