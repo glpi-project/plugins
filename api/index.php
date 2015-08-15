669ed2a63d4e74b7a0070c3e4473cbe7cd1eb0af
Nelson Zamith <nzamith@teclib.com>
2015-08-15 20:56:43 +0200

    having json answers for 404 and server errors

--- api/index.php
+++ api/index.php
@@ -6,25 +6,35 @@ require 'vendor/autoload.php';
 use \API\Core\Tool;
 $app = new \Slim\Slim();
 
-$app->notFound(function() use ($app) {
-    Tool::endWithJson([
-        "error" => "invalid endpoint"
-    ], 404);
-});
 
 // Loading all REST modules
 // with their endpoints like that:
 // inside 'src/endoints'
 $dir_endpoints = opendir('src/endpoints');
 while ($ent = readdir($dir_endpoints)) {
-	// For each .php file
-	if (preg_match('/^(.*)\.php$/', $ent, $m)) {
-		$endpoint = $m[0];
-		// Read the file with PHP
-		require 'src/endpoints/' . $endpoint;
-	}
+   // For each .php file
+   if (preg_match('/^(.*)\.php$/', $ent, $m)) {
+      $endpoint = $m[0];
+      // Read the file with PHP
+      require 'src/endpoints/' . $endpoint;
+   }
 }
 closedir($dir_endpoints);
+
+// Logs to error_log specified in virtualhost
+$app->error(function(\Exception $e) {
+   Tool::endWithJson([
+      "error" => "Server error"
+   ], 500);
+   Tool::log($e->getMessage());
+});
+
+// JSON 404 response
+$app->notFound(function() {
+    Tool::endWithJson([
+        "error" => "invalid endpoint"
+    ], 404);
+});
 
 // Ready to serve with Slim
 $app->run();
