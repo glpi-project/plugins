e19e40109ab8144ab8fb350102a9c2c37db1b749
Nelson Zamith <nzamith@teclib.com>
2015-08-15 20:55:32 +0200

    correcting a scope in Plugin that breaks count in pagination

--- api/src/models/Plugin.php
+++ api/src/models/Plugin.php
@@ -61,7 +61,7 @@ class Plugin extends Model {
    }
 
    public function scopeWithDownloads($query, $limit = false) {
-      $query->addSelect(['plugin.*', DB::raw('(SELECT COUNT(*) FROM plugin_download where plugin_download.plugin_id = plugin.id) as downloaded')])
+      $query->addSelect([DB::raw('(SELECT COUNT(*) FROM plugin_download where plugin_download.plugin_id = plugin.id) as downloaded')])
                 ->leftJoin('plugin_download', 'plugin.id', '=', 'plugin_download.plugin_id')
                 ->groupBy('plugin.name');
       return $query;
