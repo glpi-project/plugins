8184c503b9b8279caedd6daa56e1f7e1f8a8c70f
Nelson Zamith <nzamith@teclib.com>
2015-09-01 16:11:58 +0200

    preparing usage of needsScope in Author endpoint

--- api/src/endpoints/Author.php
+++ api/src/endpoints/Author.php
@@ -11,8 +11,11 @@
 
 use \API\Core\Tool;
 use \Illuminate\Database\Capsule\Manager as DB;
+use \API\OAuthServer\OAuthHelper;
 
 $all = function() use($app) {
+   OAuthHelper::needsScopes(['authors']);
+
    $all = Tool::paginateCollection(
             \API\Model\Author::mostActive()
                              ->contributorsOnly());
@@ -21,17 +24,23 @@ $all = function() use($app) {
 };
 
 $top = function() use($app) {
+   OAuthHelper::needsScopes(['authors']);
+
    $top = \API\Model\Author::mostActive(10)->get();
    Tool::endWithJson($top);
 };
 
 $single = function($id) use($app) {
+   OAuthHelper::needsScopes(['author']);
+
    $single = \API\Model\Author::withPluginCount()
                                   ->find($id);
    Tool::endWithJson($single);
 };
 
 $author_plugins = function($id) use($app) {
+   OAuthHelper::needsScopes(['author', 'plugins']);
+
    $author_plugins = \API\Model\Author::find($id);
    if (!$author_plugins)
       return Tool::endWithJson([
