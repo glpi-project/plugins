f0440c7e7ce1fb4effd32ec06d25c86f8e5f0775
Nelson Zamith <nelson@zamith.net>
2015-09-10 00:01:35 +0200

    created a dialog to link an account

--- frontend/app/scripts/controllers/panel.js
+++ frontend/app/scripts/controllers/panel.js
@@ -8,7 +8,7 @@
  * Controller of the frontendApp
  */
 angular.module('frontendApp')
-  .controller('PanelCtrl', function (API_URL, $http, $scope) {
+  .controller('PanelCtrl', function (API_URL, $http, $scope, $mdDialog, Auth) {
       $http({
          method: "GET",
          url: API_URL + '/user'
@@ -20,5 +20,26 @@ angular.module('frontendApp')
       $scope.update = function() {
          console.log('update');
          console.log($scope.user, $scope.password, $scope.password_repeat);
+      };
+
+      function LinkAccountDialogController ($scope) {
+        $scope.connect = function(provider) {
+          if (provider == 'github') {
+            Auth.linkAccount('github');
+          }
+          $mdDialog.hide();
+        };
+        $scope.close = function() {
+          $mdDialog.hide();
+        }
+      }
+      $scope.linkAccount = function(ev) {
+        $mdDialog.show({
+          controller: LinkAccountDialogController,
+          templateUrl: 'views/linkaccount.html',
+          parent: angular.element(document.body),
+          targetEvent: ev,
+          clickOutSideToClose: true
+        });
       };
   });
