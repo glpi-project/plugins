9a941267992e0fc88fa00bf1695fea386250644d
Nelson Zamith <nzamith@teclib.com>
2015-09-17 12:53:29 +0200

    transforming Tags.php to use Tool::makeEndpoint()

--- api/src/endpoints/Tags.php
+++ api/src/endpoints/Tags.php
@@ -15,7 +15,7 @@ use \API\Model\Tag;
 use \Illuminate\Database\Capsule\Manager as DB;
 use \API\OAuthServer\OAuthHelper;
 
-$tags_all = function() use ($app) {
+$tags_all = Tool::makeEndpoint(function() use ($app) {
    OAuthHelper::needsScopes(['tags']);
 
    $tags = Tool::paginateCollection(Tag::withUsage()
@@ -30,9 +30,9 @@ $tags_all = function() use ($app) {
       $tags = $tags->withLang(Tool::getRequestLang());
    }
    Tool::endWithJson(Tool::paginateCollection($tags));
-};
+});
 
-$tags_top = function() use ($app) {
+$tags_top = Tool::makeEndpoint(function() use ($app) {
    OAuthHelper::needsScopes(['tags']);
 
    $tags = Tag::withUsage()
@@ -47,9 +47,9 @@ $tags_top = function() use ($app) {
       $tags = $tags->withLang(Tool::getRequestLang());
    }
    Tool::endWithJson($tags->get());
-};
+});
 
-$tag_single = function($key) use($app) {
+$tag_single = Tool::makeEndpoint(function($key) use($app) {
    OAuthHelper::needsScopes(['tag']);
 
    $tag = Tag::where('key', '=', $key)->first();
@@ -59,9 +59,9 @@ $tag_single = function($key) use($app) {
       ], 400);
    }
   Tool::endWithJson($tag);
-};
+});
 
-$tag_plugins = function($key) use($app) {
+$tag_plugins = Tool::makeEndpoint(function($key) use($app) {
    OAuthHelper::needsScopes(['tag', 'plugins']);
 
    $tag = Tag::where('key', '=', $key)->first();
@@ -77,14 +77,13 @@ $tag_plugins = function($key) use($app) {
                 ->descWithLang(Tool::getRequestLang())
                 ->withTag($tag));
    Tool::endWithJson($plugins);
-};
+});
 
 // HTTP rest map
 $app->get('/tags', $tags_all);
 $app->get('/tags/top', $tags_top);
 $app->get('/tags/:id/plugin', $tag_plugins);
 $app->get('/tags/:id', $tag_single);
-
 
 $app->options('/tags', function(){});
 $app->options('/tags/top', function(){});
