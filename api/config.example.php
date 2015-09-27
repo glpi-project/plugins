455abe1b10f0d68305ebd45df45826d6ae998e65
Nelson Zamith <nelson@zamith.net>
2015-09-27 11:35:52 +0200

    config.example.php supports the new mail backend

--- api/config.example.php
+++ api/config.example.php
@@ -3,7 +3,7 @@
 $config = [
 	"db_settings" => [
 		'driver'    =>  'mysql',
-		'host'      =>  'localhost', 
+		'host'      =>  'localhost',
 		'database'  =>  '',
 		'username'  =>  '',
 		'password'  =>  '',
@@ -14,12 +14,17 @@ $config = [
 	"log_queries" => true,
 	"recaptcha_secret" => '--your recaptcha key here--',
 	"msg_alerts" => [
-		"recipients" => [
-			"Fullname <mail@domain>"
-		],
-		"subject_prefix" => "[GLPI PLUGINS] "
-	],
+      "smtp_server" => "smtp.server.com",
+      "transport_mode" => "tls",
+      "port" => 587,
+      "username" => "mailer@glpi-plugins.com",
+      "password" => "mailerprivatesmtppassword",
+      "local_admins" => ["person@email.com" => 'Person Full Name'], /* Used for special alerts to a admins */
+      "from" => ['mailer@glpi-plugins.com' => 'GLPi Plugins'],
+      "subject_prefix" => "[GLPI PLUGINS] "
+   ],
 	"default_number_of_models_per_page" => 15,
+   "client_url" => "http://external/path/to/glpi/client",
 	"api_url" => "http://external/path/to/api",
 	"oauth" => [ // Uncomment the following lines
 	             // to add client ID's for each
