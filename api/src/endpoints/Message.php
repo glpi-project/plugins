c1e24b15c2a67257ec5dac3f97e898dc52816ae6
Nelson Zamith <nzamith@teclib.com>
2015-09-29 10:27:59 +0200

    usage of API\Core\Mailer in Message.php

--- api/src/endpoints/Message.php
+++ api/src/endpoints/Message.php
@@ -13,15 +13,17 @@
 
 
 use \API\Core\Tool;
+use \API\Core\Mailer;
 use \Illuminate\Database\Capsule\Manager as DB;
 use \API\Model\Message;
 use \ReCaptcha\ReCaptcha;
 use \API\OAuthServer\OAuthHelper;
+use \API\Exception\InvalidField;
 
 require dirname(__FILE__) . '/../../config.php';
 
 $send = Tool::makeEndpoint(function() use($app) {
-   OAuthHelper::needsScopes(['message:send']);
+   OAuthHelper::needsScopes(['message']);
 
    $msg_alerts_settings = Tool::getConfig()['msg_alerts'];
    $body = Tool::getBody();
@@ -39,22 +41,50 @@ $send = Tool::makeEndpoint(function() use($app) {
       if (!property_exists($body->contact, $prop)) {
          Tool::endWithJson(["error" => "Missing ". $prop]);
       }
-   }
-
-   // Preparing to send mail, making recipients string
-   $recipients = ''; $i = 0;
-   foreach ($msg_alerts_settings['recipients'] as $recipient) {
-      if ($i > 0)
-         $recipients .= ', ';
-      $recipients .= $recipient;
-      $i++;
+      else {
+         switch ($prop) {
+            case 'email':
+               if (gettype($body->contact->email) != 'string' ||
+                   !filter_var($body->contact->email, FILTER_VALIDATE_EMAIL)) {
+                  throw new InvalidField('email');
+               }
+               break;
+            case 'firstname':
+               if (gettype($body->contact->firstname) != 'string' ||
+                   strlen($body->contact->firstname) > 45) {
+                  throw new InvalidField('firstname');
+               }
+               break;
+            case 'lastname':
+               if (gettype($body->contact->lastname) != 'string' ||
+                   strlen($body->contact->lastname) > 45) {
+                  throw new InvalidField('lastname');
+               }
+               break;
+            case 'subject':
+               if (gettype($body->contact->subject) != 'string' ||
+                   strlen($body->contact->subject) > 280) {
+                  throw new InvalidField('subject');
+               }
+               break;
+            case 'message':
+               if (gettype($body->contact->message) != 'string' ||
+                   strlen($body->contact->message) > 16000) {
+                  throw new InvalidField('message');
+               }
+               break;
+         }
+      }
    }
 
    // Sending mail
-   mail($recipients,
-      $msg_alerts_settings['subject_prefix'] . '[MSG] '. $body->contact->subject,
-      $body->contact->message,
-      "From: ".$body->contact->firstname." ".$body->contact->lastname." <".$body->contact->email.">");
+   $mailer = new Mailer;
+   $mailer->sendMail('user_message.html', Tool::getConfig()['msg_alerts']['local_admins'],
+                     '[MSG] '.$body->contact->subject, ['firstname' => $body->contact->firstname,
+                                                        'lastname'  => $body->contact->lastname,
+                                                        'email'     => $body->contact->email,
+                                                        'message'   => $body->contact->message],
+                     [$body->contact->email => $body->contact->firstname . ' '.$body->contact->lastname]);
 
    // also saving message in database
    $message = new Message();
