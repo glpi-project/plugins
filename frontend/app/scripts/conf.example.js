faf14778b87ab7881f1a1f3cd36d1bdb3ec4d7a1
Nelson Zamith <nzamith@teclib.com>
2015-10-06 11:52:43 +0200

    conf.example talks about github client id

--- frontend/app/scripts/conf.example.js
+++ frontend/app/scripts/conf.example.js
@@ -4,4 +4,7 @@ angular.module('frontendApp')
    // API url
    .constant('API_URL', 'http://glpiplugindirectory/api')
    // Recaptcha Public Key
-   .constant('RECAPTCHA_PUBLIC_KEY', '--your recaptcha key here--');
+   .constant('RECAPTCHA_PUBLIC_KEY', '--the recaptcha key here--')
+   // Client id of github app
+   .constant('GITHUB_CLIENT_ID', '--the github client id here--')
+;
