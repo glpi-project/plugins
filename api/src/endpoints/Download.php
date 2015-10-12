9278670a946998ce0339507a46db9b241e002331
Nelson Zamith <nzamith@teclib.com>
2015-10-12 17:58:24 +0200

    on-the-fly url correction for our day-one plugins

--- api/src/endpoints/Download.php
+++ api/src/endpoints/Download.php
@@ -23,6 +23,22 @@ $download = function($key) use($app) {
    $plugin_download->downloaded_at = DB::raw('NOW()');
    $plugin_download->plugin_id = $plugin->id;
    $plugin_download->save();
+
+   /**
+    * @MonkeyPatch
+    * @todo remove this as soon as possible once
+    * all our famous, star, since-day-one
+    * contributors took the time
+    * to update their XML file.
+    */
+   $indepnetFixSearchPattern = '/https:\/\/forge\.indepnet\.net/';
+   if (preg_match($indepnetFixSearchPattern, $plugin->download_url)) {
+      $plugin->download_url = preg_replace(
+         $indepnetFixSearchPattern,
+         'https://forge.glpi-project.org',
+         $plugin->download_url);
+   }
+
    $app->redirect($plugin->download_url, 301);
 };
 
