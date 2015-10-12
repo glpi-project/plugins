37c02c0cab65309fc1ee5267481460ef15429740
Nelson Zamith <nzamith@teclib.com>
2015-10-12 14:59:03 +0200

    First implementation of the Scope Firewall
     + which avoids scopes like "user" or "user:externalaccounts" to be
       requested as scope for an anonymous sessions (means a ["client credentials"](https://tools.ietf.org/html/rfc6749#section-1.3.4) based authorization request)
    
    Related to issue #29

--- api/src/oauthserver/AuthorizationServer.php
+++ api/src/oauthserver/AuthorizationServer.php
@@ -11,9 +11,16 @@ use \League\OAuth2\Server\Grant\PasswordGrant;
 use \League\OAuth2\Server\Grant\ClientCredentialsGrant;
 use \League\OAuth2\Server\Grant\RefreshTokenGrant;
 use \API\Model\User;
+use \API\Exception\InvalidScope;
 
 class AuthorizationServer extends \League\OAuth2\Server\AuthorizationServer {
 
+   /**
+    * This extended constructor is setting up
+    * the underlying AuthorizationServer with
+    * the grant types that GLPi Plugins support
+    * on it's OAuth2 Framework
+    */
    public function __construct() {
       parent::__construct();
       $this->setSessionStorage(OAuthHelper::getSessionStorage());
@@ -60,4 +67,42 @@ class AuthorizationServer extends \League\OAuth2\Server\AuthorizationServer {
       $this->addGrantType($refreshTokenGrant);
    }
 
+   /**
+    * The primary use case of this firewall is to
+    * limit the availability of requestable
+    * scopes in order not to allow every possible
+    * scope to client_credentials grant type
+    * requests.
+    */
+   private function firewallOnScopes($grantType, $scopes) {
+      $onlyForAuthed = ['user', 'user:externalaccounts', 'user:apps'];
+
+      if ($grantType == 'client_credentials') {
+         foreach ($onlyForAuthed as $scope) {
+            if (in_array($scope, $scopes)) {
+               throw new InvalidScope($scope);
+            }
+         }
+      }
+   }
+
+   /**
+    * This inherited method, from oauth2-server
+    * aims to provide an access_token for users
+    * when they request it. OAuth2-Server verifies
+    * accuracy of credentials, and uses it's
+    * storage-class based system which we implement
+    * using eloquent models.
+    *
+    * This method also calls firewallOnScopes()
+    * to ensure that an anonymous token is never
+    * requested with scopes like "user" or so
+    */
+   public function issueAccessToken() {
+      $grantType = $this->getRequest()->request->get('grant_type');
+      $scopes = explode(' ', $this->getRequest()->request->get('scope'));
+
+      $this->firewallOnScopes($grantType, $scopes);
+      return parent::issueAccessToken();
+   }
 }
