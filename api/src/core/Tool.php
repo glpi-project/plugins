785931890f0af8c19e8774e605b092535c88bf74
Nelson Zamith <nzamith@teclib.com>
2015-10-21 11:42:06 +0200

    support for pretty-print of api is over for now

--- api/src/core/Tool.php
+++ api/src/core/Tool.php
@@ -18,55 +18,9 @@ use \ReCaptcha\ReCaptcha;
  * used in every endpoint
  */
 class Tool {
-
    /**
-    * This template is used to deliver an API response
-    * with syntaxic coloration of JSON, in case the
-    * browser asks something else than pure
-    * application/json (in other words, in case the
-    * user directly points his browser to an API
-    * endpoint)
-    */
-   public static $prettyJSONTemplate =
-      '<!DOCTYPE html>'.
-      '<html>'.
-         '<head>'.
-            '<title></title>'.
-            '<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/8.7/styles/github.min.css">'.
-            '<script src="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/8.7/highlight.min.js"></script>'.
-            '<style>'.
-               '.hljs '.
-               '{'.
-                  'word-break: break-word;'.
-               '}'.
-               'body'.
-               '{'.
-                  'padding: 0;'.
-                  'margin: 0;'.
-                  'background-color: #f8f8f8;'.
-               '}'.
-               'pre'.
-               '{'.
-                  'margin: 0;'.
-                  'padding: 0;'.
-               '}'.
-               'pre code'.
-               '{'.
-                  'padding: 0 80px;'.
-                  'max-width: 1000px;'.
-                  'margin: 0 auto;'.
-                '}'.
-            '</style>'.
-         '</head>'.
-         '<body>'.
-            '<pre><code class="json">$code</code></pre>'.
-            '<script>hljs.initHighlightingOnLoad();</script>'.
-         '</body>'.
-      '</html>';
-
-   /**
-    * You can use this method to
-    * end with a JSON value from
+    * Method used to end with
+    * a JSON value from
     * an endpoint.
     */
    public static function endWithJson($_payload, $code = 200) {
@@ -83,25 +37,8 @@ class Tool {
          http_response_code($code);
       }
 
-      // Parsing Accept Header
-      if ($_SERVER['HTTP_ACCEPT'] == 'application/json') {
-         $acceptHtml = false;
-      }
-      else {
-         $acceptHtml = true;
-      }
-
-      if ($acceptHtml) { // if Accept header is passed
-        // we use the Javascript prettyPrint
-        $app->response->headers->set('Content-Type', 'text/html');
-        $payload = preg_replace('/\$code/',
-                             htmlentities(json_encode($payload, JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES)),
-                             self::$prettyJSONTemplate);
-         $app->halt($code, $payload);
-      } else { // else we just output
-        $app->response->headers->set('Content-Type', 'application/json');
-        $app->halt($code, json_encode($payload));
-      }
+      $app->response->headers->set('Content-Type', 'application/json');
+      $app->halt($code, json_encode($payload));
    }
 
    /**
